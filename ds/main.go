b7f21cd4d55baebd81a8d2092ff4b188fba0c1a2
--add <dan@danielms.site>
2022-04-22 20:45:06 +1000

    Update deps and improve command docs
    remove `date`, add `isosec` shortcuts

--- ds/main.go
+++ ds/main.go
@@ -7,7 +7,8 @@ import (
 	"github.com/danielmichaels/ds/pkg/install"
 	"github.com/danielmichaels/ds/pkg/scripts"
 	Z "github.com/rwxrob/bonzai/z"
-	"github.com/rwxrob/config"
+	"github.com/rwxrob/compfile"
+	"github.com/rwxrob/conf"
 	"github.com/rwxrob/help"
 	"github.com/rwxrob/uniq"
 	"github.com/rwxrob/vars"
@@ -17,11 +18,11 @@ import (
 
 func main() {
 	Z.AllowPanic = true
-	// Global alias' shortcuts for frequently used
-	Z.Aliases = map[string][]string{
-		"uuid": {"uniq", "uuid"},
-		"unix": {"scripts", "date"},
-		"env":  {"scripts", "env-check"},
+	// Global shortcuts for frequently used commands
+	Z.Shortcuts = map[string][]string{
+		"uuid":   {"uniq", "uuid"},
+		"isosec": {"uniq", "isosec"},
+		"env":    {"scripts", "env-check"},
 	}
 
 	Cmd.Run()
@@ -30,12 +31,13 @@ func main() {
 var Cmd = &Z.Cmd{
 	Name:      `ds`,
 	Summary:   `*Do Something* is a single binary to rule them all`,
-	Version:   `v0.0.5`,
+	Version:   `v0.1.0`,
 	Copyright: `Copyright 2022 Daniel Michaels`,
 	License:   `Apache-2.0`,
+	Comp:      compfile.New(),
 	Commands: []*Z.Cmd{
 		// imported
-		help.Cmd, config.Cmd, yq.Cmd, y2j.Cmd, vars.Cmd, uniq.Cmd,
+		help.Cmd, conf.Cmd, yq.Cmd, vars.Cmd, y2j.Cmd, vars.Cmd, uniq.Cmd,
 		// internal
 		scripts.Cmd, install.Cmd,
 	},
@@ -45,17 +47,29 @@ var Cmd = &Z.Cmd{
 	Description: `
 		This is my multi-call library which is set to replace the need for many
 		small bash scripts. Instead everything here will be portable and requires
-		a single curl command to pull it down on to any box. This functionality
-		contained inside is custom to my needs and may not work for anyone else.
+		a single curl command to pull it down on to any box. 
+
+		This functionality contained inside is custom to my needs and may not work for anyone else.
+
+		Intentions for **{{ .Name }}**
+
+		1. All-in-one binary
+		2. Install often used applications/binaries
+		3. Replace shell scripts on host
+
 		`,
 	Other: []Z.Section{
 		{"Shortcuts", `
-	For brevity the are several helpful shortcuts provided. These enable top level execution of commands that
-	may be several subcommands deep in the node tree.
+			For brevity the are several helpful shortcuts provided. These enable top level execution of commands that
+			may be several subcommands deep in the node tree.
 
-	"uuid": {"uniq", "uuid"},
-	"unix": {"scripts", "date"},
-	"env":  {"scripts", "env-check"},
+			**Shortcut**: Command Equivalent
+
+			* **env**:  "{{ .Name }} scripts env-check"
+			* **uuid**: "uniq uuid"
+			* **isosec**:  "uniq isosec"
+
+
 `},
 	},
 }
